a7d4ab15315b88bfb40a14c820f6adf34a4f668d
qiushui7 <107985463+qiushui7@users.noreply.github.com>
2023-09-11 20:42:18 +0800

    :bookmark: 抽离年份选择

---
+++ src/components/yearSelect.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-select
+    :model-value="props.grade"
+    @change="
+      (val) => {
+        $emit('update:grade', val);
+      }
+    "
+    style="width: 140px; margin-right: 10px"
+  >
+    <el-option v-for="item in grades" :key="item.value" :label="item.value" :value="item.value" />
+  </el-select>
+</template>
+
+<script setup lang="ts">
+import { onMounted, reactive, watch } from "vue";
+interface Props {
+  grade: number;
+}
+let props = defineProps<Props>();
+const emit = defineEmits(["update:grade"]);
+const grades = reactive([]);
+let getGrades = () => {
+  const now = new Date();
+  const year = 2019;
+  emit("update:grade", year);
+  for (let i = 0; i < 5; i++) {
+    grades.push({
+      value: year - i,
+    });
+  }
+};
+onMounted(() => {
+  getGrades();
+});
+</script>
+
+<style scoped lang="less"></style>
